878e766426ff78d371bfc45d006e59958f9d2744
Przemek Wiech <pwiech@gmail.com>
2016-09-14 23:25:08 +0200

    Added buttons to show a random node, way or relation.

--- app.js
+++ app.js
@@ -33,13 +33,15 @@ getBounds = function(nodes) {
 /**
  * Controller for the home page.
  */
-HomeCtrl = function($rootScope, $location) {
+HomeCtrl = function($rootScope, $location, $http, $window) {
   $rootScope.title = 'OSM history';
   this.wayId;
   this.nodeId;
   this.relationId;
 
   this.ngLocation = $location;
+  this.ngHttp = $http;
+  this.ngWindow = $window;
 };
 
 
@@ -49,6 +51,68 @@ HomeCtrl = function($rootScope, $location) {
 HomeCtrl.prototype.showHistory = function(type, id) {
   if (id) {
     this.ngLocation.path(`/${type}/${id}`);
+  }
+};
+
+
+/**
+ * Navigates to the history page of a random object of the given type.
+ * Sends 5 requests to get a random object in parallel. If an object with at
+ * least 3 versions is found, its history is shown. Otherwise any other
+ * object that actually exists is shown. If all requests fail, the history of
+ * one of hard-coded objects is shown.
+ */
+HomeCtrl.prototype.randomize = function(type) {
+  this.ngWindow.ga('send', 'event', 'random', type);
+  // Max ID for random generation.
+  var MAX_ID = {
+    'node': 1000000000,
+    'way': 400000000,
+    'relation': 1000000
+  };
+  // If all fails, fall back to one of these objects.
+  var DEFAULT_ID = {
+    'node': 1804402734,
+    'way': 26129870,
+    'relation': 2308603
+  };
+  var maybeShow = null;
+  var receivedCount = 0;
+  var done = false;
+
+  // Number of random requests to make to find a good object.
+  var NUM_TRIES = 5;
+
+  for (let i = 0; i < NUM_TRIES; i++) {
+    var id = Math.floor(Math.random() * MAX_ID[type]) + 1;
+    var url = `${API_URL_BASE}${type}/${id}`;
+    fetchOsm(this.ngHttp, url, type).then((obj) => {
+      if (done) {
+        // Already navigated.
+        return;
+      }
+      if (parseInt(obj[0]._version) >= 3) {
+        // Navigating to object with at least 3 versions.
+        this.showHistory(type, obj[0]._id);
+        done = true;
+      } else {
+        // Found object with less than 3 versions. If nothing better is found,
+        // use this one.
+        maybeShow = obj[0]._id;
+      }
+    }).finally(() => {
+      receivedCount++;
+      if (!done && receivedCount == NUM_TRIES) {
+        // All requests finished but no good object was found.
+        if (maybeShow) {
+          // Navigate to object with small history.
+          this.showHistory(type, maybeShow);
+        } else {
+          // Navigate to default object because all random tries have failed.
+          this.showHistory(type, DEFAULT_ID[type]);
+        }
+      }
+    });
   }
 };
 
